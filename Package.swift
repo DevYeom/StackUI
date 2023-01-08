ac07754d5f95abdcd74b47c82dbe4ce3f9c095db
DevYeom <dev.yeom@gmail.com>
2023-01-08 21:13:19 +0900

    Add SnapKit dependency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,25 +6,22 @@ import PackageDescription
 let package = Package(
     name: "StackUI",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v11),
         .macOS(.v10_11),
-        .tvOS(.v9),
+        .tvOS(.v11),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "StackUI",
-            targets: ["StackUI"]
-        ),
+        .library(name: "StackUI", targets: ["StackUI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
         .target(
             name: "StackUI",
-            dependencies: []
+            dependencies: [
+                .product(name: "SnapKit", package: "SnapKit"),
+            ]
         ),
         .testTarget(
             name: "StackUITests",
